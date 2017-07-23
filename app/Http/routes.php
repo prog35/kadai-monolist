6972fc7237c8b4120974e2cd97ceb2c33f0535cb
taka8 <learning.prog35@gmail.com>
2017-07-23 06:12:53 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,3 +13,5 @@
 
 Route::get('/', 'WelcomeController@index');
 
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
